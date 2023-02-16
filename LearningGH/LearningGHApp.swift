8f39c4d690eecafa419e24b2455cf7b0350daace
Lee4-ux <ljcob@protonmail.com>
2023-02-16 12:27:52 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by LJ Jacobs on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
